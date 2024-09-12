2528bca7d8f97da3688f025762490025ce68532e
ehsanodyssey <ehsan.odyssey@gmail.com>
2024-09-13 02:17:46 +0330

    impression and click services and init data loader have been implemented

--- src/main/java/neo/ehsanodyssey/advertisement/AdvertisementAPI.java
+++ src/main/java/neo/ehsanodyssey/advertisement/AdvertisementAPI.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author : EhsanOdyssey (AmirEhsan Shahmirzaloo)
  * @mailto : <a href="mailto:ehsan.shahmirzaloo@gmail.com">EhsanOdyssey</a>
  * @project : advertisement
- * @created : 2023-12-31 Dec/Sun
+ * @created : 2024-09-12 Sep/Thu
  **/
 @SpringBootApplication
 public class AdvertisementAPI {
